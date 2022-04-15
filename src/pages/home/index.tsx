8ce98205b7a6a894de75bd58280487922a05dbff
Michael Loewe <loewe.alivio@gmail.com>
2022-04-15 23:28:41 +0800

    💄 🚸 Added styling and content in Home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,48 @@
 import * as React from "react"
+import styled from "styled-components/macro"
+import { keyframes } from "styled-components"
+
+import { colors } from "src/theme"
 
 function Home() {
   return (
-    <div>
-      <p>Hello test</p>
-    </div>
+    <Section>
+      <H1>Hi, I'm Michael Loewe Alivio</H1>
+      <P>I'm a developer situated on Philippines — honing my coding skills.</P>
+    </Section>
   )
 }
+
+const fromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  animation: ${fromLeft} 2.5s ease;
+`
+
+const H1 = styled.h1`
+  font-family: "Montserrat";
+  font-size: 6rem;
+  font-weight: 600;
+  color: ${colors.hyperlink};
+`
+
+const P = styled.p`
+  font-family: "Roboto";
+  font-size: 2rem;
+  font-weight: normal;
+  color: ${colors.text};
+`
 
 export default Home
