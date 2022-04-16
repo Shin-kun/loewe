937b29432519a5cd689ef56d74cba829c629958b
Michael Loewe <loewe.alivio@gmail.com>
2022-04-16 17:52:05 +0800

    🚚 ♻️ Refactored and moved navbar to layout instead

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -9,6 +9,9 @@ import * as React from "react"
 import styled from "styled-components/macro"
 
 import Header from "../header"
+import NavContext from "src/hooks/nav/context"
+import useNav from "src/hooks/nav"
+import Navbar from "src/components/navbar"
 import "./styles.css"
 
 interface LayoutProps {
@@ -19,23 +22,32 @@ const Layout: React.FC<React.PropsWithChildren<LayoutProps>> = ({
   className,
   children,
 }) => {
+  const [isNavButtonClicked, onNavButtonClicked] = useNav()
+
   return (
     <Container className={className}>
-      <Header />
-      <ContainerWithMargin
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <Main>{children}</Main>
-      </ContainerWithMargin>
+      <NavContext.Provider value={{ isNavButtonClicked, onNavButtonClicked }}>
+        <Header />
+        <ContainerWithMargin
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0 1.0875rem 1.45rem`,
+          }}
+        >
+          <Main>
+            <Navbar />
+            {children}
+          </Main>
+        </ContainerWithMargin>
+      </NavContext.Provider>
     </Container>
   )
 }
 
-const Container = styled.div``
+const Container = styled.div`
+  overflow: hidden;
+`
 
 const Main = styled.main``
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,41 @@
 import * as React from "react"
-import styled from "styled-components"
+import styled from "styled-components/macro"
+import { keyframes } from "styled-components"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Home from "./home"
-import useNav from "../hooks/nav"
-import NavContext from "../hooks/nav/context"
-import Navbar from "src/components/navbar"
+import { colors } from "src/theme"
 
 const IndexPage = () => {
-  const [isNavButtonClicked, onNavButtonClicked] = useNav()
-
   return (
-    <NavContext.Provider value={{ isNavButtonClicked, onNavButtonClicked }}>
+    <>
       <Seo title="Home" />
       <Container>
-        <Navbar />
         <Home />
       </Container>
-    </NavContext.Provider>
+    </>
   )
 }
 
+const fromBottom = keyframes`
+  from {
+    opacity: 0;
+    transform: scaleY(0);
+  }
+
+  to {
+    opacity: 1;
+    transform: scaleY(1);
+  }
+`
+
 const Container = styled(Layout)`
-  background-color: #0b0c10;
+  background-color: ${colors.primary};
   height: 100vh;
   width: 100vw;
+  transform-origin: 0% bottom;
+  animation: ${fromBottom} 0.75s ease;
 `
 
 export default IndexPage
