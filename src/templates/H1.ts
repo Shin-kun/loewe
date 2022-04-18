e4800559ba0e1e41d20bc1b6815bc9287b95f8c9
Michael Loewe <loewe.alivio@gmail.com>
2022-04-18 16:47:02 +0800

    💄 Modified h1 font family and weight

--- src/templates/H1.ts
+++ src/templates/H1.ts
@@ -3,9 +3,9 @@ import styled from "styled-components/macro"
 import { colors, device } from "src/theme"
 
 const H1 = styled.h1`
-  font-family: "Montserrat";
+  font-family: "Lora";
   font-size: 4.5rem;
-  font-weight: 900;
+  font-weight: 700;
   color: ${colors.hyperlink};
 
   @media ${device.tablet} {
