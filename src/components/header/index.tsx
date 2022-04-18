7887f53184fe231def1920523726f3dec3caa524
Michael Loewe <loewe.alivio@gmail.com>
2022-04-19 00:04:57 +0800

    ✨ Added a link to home page in the header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,19 @@
-import React from "react"
+import React, { useContext } from "react"
 import styled from "styled-components/macro"
 
 import Nav from "./components/nav"
+import A from "src/templates/Link"
+import NavContext, { NavContextProps } from "src/hooks/nav/context"
 
 function Header() {
+  const navContext = useContext(NavContext)
+  const { isNavButtonClicked } = navContext as NavContextProps
+
   return (
     <Container>
+      <HomeLink isNavButtonClicked={isNavButtonClicked} to="/">
+        LW
+      </HomeLink>
       <Nav />
     </Container>
   )
@@ -17,8 +25,22 @@ const Container = styled.header`
   padding: 3rem;
   position: sticky;
   background-color: transparent;
-  justify-content: flex-end;
   z-index: 21;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+const HomeLink = styled(A)<{ isNavButtonClicked: boolean }>`
+  font-family: "Anton";
+  font-size: 2rem;
+
+  visibility: ${({ isNavButtonClicked }) =>
+    isNavButtonClicked ? "hidden" : "visible"};
+
+  &:hover {
+    text-decoration: line-through;
+    text-decoration-thickness: 3px;
+  }
 `
 
 export default Header
