182e9e0d5a8379cf5595df37aadc7d720ee37401
Michael Loewe <loewe.alivio@gmail.com>
2022-04-18 12:43:13 +0800

    🧱 Added expected device sizes for media queries

---
+++ src/theme/colors.ts
@@ -0,0 +1,9 @@
+const colors = {
+  primary: "#0B0C10",
+  secondary: "#1F2833",
+  text: "#C5C6C7",
+  hyperlink: "#66FCF1",
+  buttons: "#45A29E",
+}
+
+export default colors
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,7 +1,4 @@
-export const colors = {
-  primary: "#0B0C10",
-  secondary: "#1F2833",
-  text: "#C5C6C7",
-  hyperlink: "#66FCF1",
-  buttons: "#45A29E",
-}
+import colors from "./colors"
+import device from "./device"
+
+export { colors, device }
