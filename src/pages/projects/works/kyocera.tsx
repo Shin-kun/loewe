3fd58147149c0ed1f3cbdec85330adb93d72d006
Michael Loewe <loewe.alivio@gmail.com>
2022-04-18 18:06:50 +0800

    📱 Improved kyocera page by making it more responsive to mobile design

--- src/pages/projects/works/kyocera.tsx
+++ src/pages/projects/works/kyocera.tsx
@@ -1,15 +1,11 @@
 import React from "react"
 import styled from "styled-components/macro"
-import { keyframes } from "styled-components"
 import { StaticImage } from "gatsby-plugin-image"
 
-import H1 from "src/templates/H1"
 import Seo from "src/components/seo"
-import H5 from "src/templates/H5"
-import Text from "src/components/text"
 import A from "src/templates/Link"
 import { Container } from "../components/container"
-import { colors } from "src/theme"
+import { colors, device } from "src/theme"
 import { ImageContainer } from "../components/image-container"
 import {
   TitleContainer,
@@ -17,19 +13,22 @@ import {
   JobOverviewSection,
   JobOverviewSubSection,
 } from "../templates/container"
+import { HeaderSection } from "src/templates/section"
+import { Title, Heading, JobOverviewHeading } from "../templates/heading"
+import { Paragraph, JobOverviewSummary } from "../templates/paragraph"
 
 function Kyocera() {
   return (
     <Container>
       <Seo title="Kyocera" />
-      <TitleContainer>
+      <HeaderSection>
         <Title>Kyocera</Title>
-      </TitleContainer>
-      <Overview>
         <Heading>
-          A Linux driver desktop app that communicates/sends user commands to a
-          Kyocera printer — C++, QT Creator, etc.
+          A Linux driver desktop app that communicates or sends user commands to
+          a Kyocera printer.
         </Heading>
+      </HeaderSection>
+      <Overview>
         <JobOverviewSection>
           <JobOverviewSubSection>
             <JobOverviewHeading>Role</JobOverviewHeading>
@@ -63,56 +62,17 @@ function Kyocera() {
           support new features that were in demand by the customers.
         </Paragraph>
         <CompanyLinkContainer>
-          <Heading>Check the main page.</Heading>
-          <Link to="https://www.kyoceradocumentsolutions.com/en/index.html">
-            https://www.kyoceradocumentsolutions.com/
-          </Link>
+          <Heading>
+            Check them out
+            <Link to="https://www.kyoceradocumentsolutions.com/en/index.html">
+              here
+            </Link>
+          </Heading>
         </CompanyLinkContainer>
       </Overview>
     </Container>
   )
 }
-
-const fromLeft = keyframes`
-  from {
-    opacity: 0;
-    transform: translateX(-100%);
-  }
-
-  to {
-    opacity: 1;
-    transform: translateX(0);
-  }
-`
-
-const Title = styled(H1)`
-  animation: ${fromLeft} 1.5s ease;
-`
-
-const Heading = styled(H5)`
-  text-align: justify;
-  margin-bottom: 2rem;
-  color: ${colors.buttons};
-  animation: ${fromLeft} 2s ease;
-`
-
-const JobOverviewHeading = styled(Text)`
-  font-size: 1rem;
-  font-weight: bold;
-  text-decoration: underline;
-  text-transform: uppercase;
-  margin-bottom: 1rem;
-`
-
-const JobOverviewSummary = styled(Text)`
-  font-size: 1rem;
-  font-weight: 400;
-  margin: 0;
-`
-
-const Paragraph = styled(JobOverviewSummary)`
-  text-align: justify;
-`
 
 const CompanyLinkContainer = styled(JobOverviewSubSection)`
   align-self: center;
@@ -121,18 +81,22 @@ const CompanyLinkContainer = styled(JobOverviewSubSection)`
 
 const Link = styled(A)`
   width: fit-content;
-  font-size: 1rem;
+  font-size: 2rem;
+  font-weight: 700;
   color: ${colors.hyperlink};
-  background-color: ${colors.primary};
-  font-weight: bold;
-  padding: 1.5rem;
+  padding: 1rem;
   border-radius: 2rem;
   transition: all 0.5s;
 
+  @media ${device.tablet} {
+    font-size: 3rem;
+  }
+
   &:hover {
     cursor: pointer;
-    padding-left: 3rem;
-    padding-right: 3rem;
+    font-size: 3.25rem;
+    /* text-decoration: line-through;
+    text-decoration-thickness: 5px; */
   }
 `
 
