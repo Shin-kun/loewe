49e6c93be5486c4d39f8b5bd9e868c0c2d9b2c3a
Michael Loewe <loewe.alivio@gmail.com>
2022-04-19 00:09:04 +0800

    📱 Improved header by making it more responsive to mobile design

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,7 +3,9 @@ import styled from "styled-components/macro"
 
 import Nav from "./components/nav"
 import A from "src/templates/Link"
-import NavContext, { NavContextProps } from "src/hooks/nav/context"
+import NavContext from "src/hooks/nav/context"
+import { NavContextProps } from "src/hooks/nav/context"
+import { device } from "src/theme"
 
 function Header() {
   const navContext = useContext(NavContext)
@@ -22,12 +24,16 @@ function Header() {
 const Container = styled.header`
   display: flex;
   width: 100%;
-  padding: 3rem;
   position: sticky;
   background-color: transparent;
   z-index: 21;
   flex-direction: row;
   justify-content: space-between;
+  padding: 3rem 1.0875rem 1.45rem;
+
+  @media ${device.laptop} {
+    padding: 3rem;
+  }
 `
 
 const HomeLink = styled(A)<{ isNavButtonClicked: boolean }>`
