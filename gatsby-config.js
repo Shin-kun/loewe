ab364cf0d05934ad17e020fef90f26b5daf21638
Michael Loewe <loewe.alivio@gmail.com>
2022-04-19 00:04:27 +0800

    🍱 Added favicon icon for website

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,12 +24,9 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        background_color: `#0B0C10`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/loewe-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
