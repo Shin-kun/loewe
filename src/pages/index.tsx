fb4455c9b18c99121a659d2ab350f730319157b2
Michael Loewe <loewe.alivio@gmail.com>
2022-04-19 14:57:58 +0800

    🚚 Moved home component to display in the root index instead

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,20 @@ import { keyframes } from "styled-components"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import { Home } from "./home"
-import { colors } from "src/theme"
+import H1 from "src/styled-components/H1"
+import H5 from "src/styled-components/H5"
+import { colors, device } from "src/theme"
 
 const IndexPage = () => {
   return (
     <Container>
       <Seo title="Home" />
-      <Home />
+      <Section>
+        <Heading>Hi, I'm Michael Loewe Alivio</Heading>
+        <SubHeading>
+          A Frontend Web & Mobile Developer from Cebu, Philippines
+        </SubHeading>
+      </Section>
     </Container>
   )
 }
@@ -33,6 +39,42 @@ const Container = styled(Layout)`
   transform-origin: 0% bottom;
   animation: ${fromBottom} 2s ease;
   overflow: auto;
+`
+
+const fromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`
+
+const Section = styled.section`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`
+
+const Heading = styled(H1)`
+  font-weight: bold;
+  color: ${colors.hyperlink};
+  font-size: 4rem;
+  animation: ${fromLeft} 1.5s ease;
+
+  @media ${device.laptop} {
+    font-size: 7rem;
+  }
+`
+
+const SubHeading = styled(H5)`
+  font-weight: normal;
+  line-height: 1.5;
+  animation: ${fromLeft} 2.5s ease;
 `
 
 export default IndexPage
