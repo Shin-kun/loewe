ea198bb6bfa75df0bae5092e2d228c9ac4f340fc
Michael Loewe <loewe.alivio@gmail.com>
2022-04-19 13:14:40 +0800

    🔥 Removed TitleContainer as an import

--- src/pages/projects/components/container.tsx
+++ src/pages/projects/components/container.tsx
@@ -1,10 +1,6 @@
 import React from "react"
 
-import {
-  MainContainer,
-  TitleContainer,
-  MainSection,
-} from "../templates/container"
+import { MainContainer, MainSection } from "../templates/container"
 
 interface ContainerProps {
   className?: string
