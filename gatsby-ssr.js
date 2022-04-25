6d7c1ff801025dbccf9d52650e3c26c191612ead
Michael Loewe <loewe.alivio@gmail.com>
2022-04-25 22:01:11 +0800

    💄 Updated preloader UI

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,10 +4,14 @@ exports.onRenderBody = ({
   setPreBodyComponents,
   setHeadComponents,
   setPostBodyComponents,
+  setBodyAttributes,
 }) => {
   setPreBodyComponents([<div id="preloader"></div>])
   setHeadComponents([
     <link as="script" rel="preload" href="/scripts/preloader.js" />,
   ])
   setPostBodyComponents([<script src="/scripts/preloader.js" />])
+  setBodyAttributes({
+    className: "preloader-active",
+  })
 }
